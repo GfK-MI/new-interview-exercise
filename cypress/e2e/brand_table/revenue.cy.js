27aa10368e1c735f9b1e8fece445747427680cf1
Mikhail Belyaev <mikhail.belyaev@gfk.com>
2024-02-16 18:13:06 +0100

    chore: upgrade cypress to 13.6.4
    * Upgraded cypress to 13.6.4.
    * Generated new examples.

---
+++ cypress/e2e/brand_table/revenue.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+context("Actions", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("WHEN I load the brand table, THEN the table contains 5 rows", () => {
+    cy.get(".MuiTableBody-root").find("tr").should("have.length", 5);
+  });
+});
